d60c9de280935e9b05f95ee503320a09687914b9
misaellimax <99142847+Misaellimax@users.noreply.github.com>
2023-05-08 20:23:41 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ inputQuestion.addEventListener("keypress", (e) => {
   if (inputQuestion.value && e.key === "Enter") SendQuestion();
 });
 
-const OPENAI_API_KEY = "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";
+const OPENAI_API_KEY = "";
 
 function SendQuestion() {
   var sQuestion = inputQuestion.value;
